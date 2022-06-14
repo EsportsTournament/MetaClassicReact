bf2c4686a1b114f5656561a031ef5638b0ff31c0
shresth7001 <shresth7001@gmail.com>
2022-06-14 14:12:34 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+import { GlobalStyle } from './globalStyles';
+import './App.scss';
+
+function App() {
+	return (
+		<Fragment>
+			<GlobalStyle />
+			<div className="App">Welcome to meta classic esports tournament</div>
+		</Fragment>
+	);
+}
+
+export default App;
