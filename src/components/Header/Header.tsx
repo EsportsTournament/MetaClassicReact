bbe8c9a6a288586432731aff03ffb80a8e091e01
aditya-thakur <adityathakur532@gmail.com>
2022-07-15 00:43:03 +0530

    minor update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,26 +4,29 @@ import bgImg from "../../assets/images/logo2.png";
 import {RiAccountCircleFill} from "react-icons/ri";
 import axios from "axios";
 const Header: FC = () => {
+  const backendURL = "https://user-manager-meta.herokuapp.com";
   const appName = "Meta Esports";
   const [me, setMe] = useState(null);
   const [loginURL, setLoginURL] = useState("https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=https%3A%2F%2Fuser-manager-meta.herokuapp.com%2Fauth%2Fgoogle&client_id=888992868623-g5ffjm72bs7sli419ksdq44iinhml7kv.apps.googleusercontent.com&access_type=offline&response_type=code&prompt=consent&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email");
+  // const [loginURL, setLoginURL] = useState("https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=http%3A%2F%2Flocalhost%3A4000%2Fauth%2Fgoogle&client_id=888992868623-g5ffjm72bs7sli419ksdq44iinhml7kv.apps.googleusercontent.com&access_type=offline&response_type=code&prompt=consent&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email");
+  
+  async function getLoginURL(){
+    await axios
+    .get(`${backendURL}/auth/google/url`, {
+      withCredentials: true,
+    })
+    .then((res) => setLoginURL(res.data));
+  }
+  async function getMe() {
+    await axios
+      .get(`${backendURL}/auth/me`, {
+        withCredentials: true,
+      })
+      .then((res) => setMe(res.data));
+  }
 
   useEffect(() => {
-    // async function getLoginURL() {
-    //   await axios
-    //   .get("https://user-manager-meta.herokuapp.com/auth/google/url", {
-    //     withCredentials: true,
-    //   })
-    //   .then((res) => setLoginURL(res.data));
-    // }
-    async function getMe() {
-      await axios
-        .get("https://user-manager-meta.herokuapp.com/auth/me", {
-          withCredentials: true,
-        })
-        .then((res) => setMe(res.data));
-    }
-    
+    getLoginURL();
     getMe();
   }, []);
 
