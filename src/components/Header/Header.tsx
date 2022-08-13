87a1c777ecdd0751154689def4dc52e33b0e547f
aditya-thakur <adityathakur532@gmail.com>
2022-08-13 10:40:15 +0530

    header fixed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,8 +5,9 @@ import { RiAccountCircleFill } from "react-icons/ri";
 import axios from "axios";
 import { User } from "../../models/user";
 const Header: FC = () => {
-  const backendURL = "https://user-manager-meta.herokuapp.com";
-  const appName = "Meta Esports";
+  // const backendURL = "https://user-manager-meta.herokuapp.com";
+  const backendURL = "http://192.168.1.4:3000";
+  const appName = "Meta";
   const [me, setMe] = useState({} as User);
   const [loginURL, setLoginURL] = useState("https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=https%3A%2F%2Fuser-manager-meta.herokuapp.com%2Fauth%2Fgoogle&client_id=888992868623-g5ffjm72bs7sli419ksdq44iinhml7kv.apps.googleusercontent.com&access_type=offline&response_type=code&prompt=consent&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email");
 
@@ -42,7 +43,7 @@ const Header: FC = () => {
         (<img className="profileImage" src={me.picture} alt="profile image" />):
         (<RiAccountCircleFill />) }
         {me.name ? (<div className="userName">{me.name}</div>) : <a href={loginURL}>
-          LOGIN WITH GOOGLE
+          LOGIN
         </a>}
       </div>
     </div>
