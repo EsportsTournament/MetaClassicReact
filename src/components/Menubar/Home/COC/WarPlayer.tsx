fbf4806468dc132f6ffd3ea8e1de2cf4147d42c4
aditya-thakur <adityathakur532@gmail.com>
2022-09-02 17:14:25 +0530

    war page halfway done

--- src/components/Menubar/Home/COC/WarPlayer.tsx
+++ src/components/Menubar/Home/COC/WarPlayer.tsx
@@ -1,11 +1,31 @@
 import React from "react";
 
 function WarPlayer(props: any) {
-  return (
-   <div className="warPlayer">
-    {props.player.name} ने {props.player.score}% स्कोर प्राप्त किया॰
-   </div>
-  );
+    return (
+        <div className="warPlayer">
+            <div className="member">
+                <img src={require(`./../../../../assets/images/th/${props.player.th}.webp`)} alt="town hall iamge" />
+                {props.player.name} ({props.player.pos})
+            </div>
+            <div className="attacks">
+                {props.player.attacks.map( (attack: any, index: number) => {
+                    return <div className="attack" key={index}>
+                        {attack.stars === 3 ? 
+                         <img src={require("./../../../../assets/images/th/_3.webp")} alt="town hall iamge" />
+                       :  <img src={require(`./../../../../assets/images/th/${attack.defenderTh}_${attack.stars}.webp`)} alt="town hall iamge" />
+                    }
+                        {attack.defenderName} ({attack.defenderMapPosition})
+                    </div>;
+                })}
+            </div>
+            <div className="score-label">
+                <span>Total Score</span> 
+                <span className="score">
+                    {props.player.score}
+                </span>
+            </div>
+        </div>
+    );
 }
 
 export default WarPlayer;
